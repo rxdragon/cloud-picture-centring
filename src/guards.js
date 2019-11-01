9c6508b41aa47a03ae5ed660e27dd5de56149b4e
崔佛 <1406211263@qq.com>
2019-11-01 09:58:00 +0800

    防止路由重复加载

--- src/guards.js
+++ src/guards.js
@@ -22,6 +22,7 @@ router.beforeEach(async (to, from, next) => {
       NProgress.done()
     } else {
       const nickname = store.getters.nickname
+      console.log(nickname, 'nickname')
       if (nickname) {
         next()
       } else {
