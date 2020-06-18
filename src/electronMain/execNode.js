51733a199deaf3a01b8faaca95fb56c876d5f5e4
cf <1406211263@qq.com>
2020-06-18 19:58:12 +0800

    feat: 获取ps内存

--- src/electronMain/execNode.js
+++ src/electronMain/execNode.js
@@ -22,6 +22,30 @@ function initExecIncident (win, ipcMain) {
       win.webContents.send('find-photoshop:res', { stdout })
     })
   }
+  // 获取PS内存
+  function getPSMemory () {
+    execute("ps -A -o ppid,command | grep Photoshop | awk '{ print $1 }'", (error, stdout, stderr) => {
+      if (error) {
+        win.webContents.send('get-photoshop-memory:res', { error })
+        return
+      }
+      const ppids = stdout
+      const ppidArr = ppids.split('\n')
+      const d = {}
+      ppidArr.forEach(k => {
+        !d[k] ? d[k] = 1 : d[k]++
+      })
+      const mainPid = Object.keys(d).sort((a, b) => d[b] - d[a])[0]
+      execute(`ps Au -o %cpu,%mem,ppid | grep ${mainPid} | awk '{ print $2, $3, $4, $5, $6, $14 }'`, (error, stdout, stderr) => {
+        if (error) {
+          win.webContents.send('get-photoshop-memory:res', { error })
+          return
+        }
+        win.webContents.send('get-photoshop-memory:res', { stdout })
+      })
+    })
+  }
+  ipcMain.on('get-photoshop-memory', getPSMemory)
   ipcMain.on('find-photoshop', findPhotoShop)
 }
 
