ef38bb862b251d295def6d8a9b043cdf75a1ea77
cf <1406211263@qq.com>
2020-08-14 11:41:09 +0800

    fix: 修复部分bug

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -1,12 +1,5 @@
 <template>
   <div class="home">
-    <div v-if="upload" id="app">
-      <div class="onUpload">
-        <p>检查到版本更新</p>
-        <p class="version">版本号: <span>{{ upload }}</span></p>
-        <button @click="doUpgrade">立即更新</button>
-      </div>
-    </div>
     <div class="logo-tip">
       <div class="logo-img home-logo" />
       <div class="logo-desc">
@@ -18,31 +11,14 @@
 </template>
 
 <script>
-const { ipcRenderer } = window.require('electron')
-
 export default {
   name: 'Home',
   data () {
-    return {
-      upload: '' // 是否有版本更新
-    }
+    return {}
   },
-  mounted () {
-    ipcRenderer.on('version:find-new', (event, info) => {
-      this.upload = info.newVersion
-    })
-  },
-  methods: {
-    async doUpgrade () {
-      await ipcRenderer.send('version:do-upgrade')
-      this.$newMessage.success('更新成功')
-    }
-  }
 }
 </script>
 <style lang="less" scoped>
-
-
 .home {
   position: relative;
   height: @homeHeight;
