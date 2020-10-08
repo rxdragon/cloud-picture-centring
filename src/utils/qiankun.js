313eeaf22cb4d5ba6989fdd2de306d22a9e1ef86
cf <1406211263@qq.com>
2020-10-08 10:08:08 +0800

    feat: 更改微前端线上地址

--- src/utils/qiankun.js
+++ src/utils/qiankun.js
@@ -9,8 +9,7 @@ const entryMap = {
   'pictureonline': {
     local: 'http://localhost:8081',
     development: 'http://fed.dev.hzmantu.com/picture-online-web/index.html',
-    // TODO
-    production: '//release_2_0_0-himo_picture_online_web_v2.fed-dev.hzmantu.com',
+    production: 'https://picture.hzmantu.com/picture-online-web/index.html',
   }
 }
 
