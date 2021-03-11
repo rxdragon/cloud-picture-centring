c120d132c49979d696e87fb475c9aa7f7e2684d0
Trevor <1406211263@qq.com>
2021-03-11 10:54:53 +0800

    feat: 添加相关页面

---
+++ src/views/showpic-assessment-center/showpic-assessment-history.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="showpic-assessment-history">
+    修修兽评价历史记录
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ShowpicAssessmentHistory'
+}
+</script>
---
+++ src/views/showpic-assessment-center/showpic-grade-configuration.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="showpic-grade-configuration">
+    修修兽评分配置
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ShowpicGradeConfiguration'
+}
+</script>
