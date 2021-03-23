3995be776597948dbf0593a26eec13070d14b4a4
Trevor <1406211263@qq.com>
2021-03-23 11:28:15 +0800

    feat: 增加相近色洋色

---
+++ src/components/vCharts/components/data-empty.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="v-charts-data-empty">
+    暂无数据
+  </div>
+</template>
+
+<style>
+.v-charts-data-empty {
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 14px;
+  color: #888;
+  background-color: rgba(255, 255, 255, 0.9);
+}
+</style>
