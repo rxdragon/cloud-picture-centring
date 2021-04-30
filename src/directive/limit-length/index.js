cbef6e4c57ec1eeb3874213a26be0d66213f387a
Trevor <1406211263@qq.com>
2021-04-30 11:04:57 +0800

    fix: 修复输入法长度限制导致的bug

--- src/directive/limit-length/index.js
+++ src/directive/limit-length/index.js
@@ -3,13 +3,19 @@ import Vue from 'vue'
 Vue.directive('maxLength16', {
   inserted: (el, bind) => {
     el.addEventListener('keyup', (value) => {
-      const reg = /^[a-zA-Z0-9\u4e00-\u9fa5\s]{0,16}$/g
-      const reg2 = /[a-zA-Z0-9\u4e00-\u9fa5\s]{0,16}/g
+      const reg = /^[a-zA-Z0-9\u4e00-\u9fa5\s']{0,16}$/g
+      const reg2 = /[a-zA-Z0-9\u4e00-\u9fa5\s']{0,16}/g
       const has = !reg.test(event.target.value)
+      if (el.__vue__) {
+        el.__vue__.isComposing = false
+      }
       if (has) {
         const regArr = reg2.exec(event.target.value)
         const trueText = regArr ? regArr[0] : ''
         event.target.value = trueText
+        if (el.__vue__) {
+          el.__vue__.isComposing = false
+        }
         event.target.dispatchEvent(new Event('input'))
       }
     })
