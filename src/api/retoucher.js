6da37c8598b800a2dbc4f67ac17084b7c7939fce
Trevor <1406211263@qq.com>
2021-05-31 19:01:23 +0800

    fix: 更改小于0 为显示0

--- src/api/retoucher.js
+++ src/api/retoucher.js
@@ -94,6 +94,7 @@ export function getSelfQuota () {
     todayTargetPhotoNum(data.predictFloatPhotoNum)
     todayTargetPhotoNum(-1 * data.vacateReducePhotoNum)
     data.todayTargetPhotoNum = todayTargetPhotoNum.toResult()
+    data.todayTargetPhotoNum = data.todayTargetPhotoNum <= 0 ? 0 : data.todayTargetPhotoNum
 
     const todayFinishPhotoNumProgress = getAvg(data.todayAllFinishPhotoNum, data.todayTargetPhotoNum)
     data.todayFinishPhotoNumProgress = todayFinishPhotoNumProgress * 100
